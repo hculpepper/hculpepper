63b4120e275ea039c1c003fba6144f3b7afc8ce6
Houston Culpepper <hculpepper@my.waketech.edu>
2025-04-10 18:03:14 -0400

    corrected console log and change table header color

--- gradebook.js
+++ gradebook.js
@@ -1,11 +1,13 @@
 // TODO: Fetch data from PostgreSQL database (later)
 function fetchGradeData() {
+  // This function will query the PostgreSQL database and return grade data
   console.log("Fetching grade data...");
 }
 
 // TODO: Populate the table with grade data
 function populateGradeTable(data) {
-  console.log("Populating grade table with data...");
+  // This function will take the fetched data and populate the table
+  console.log("Populating grade table with data...", data);
 }
 
 // TODO REMOVE THIS
