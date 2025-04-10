3ef05422cd47a89be7e7ad9594f10f0af946e297
Houston Culpepper <hculpepper@my.waketech.edu>
2025-04-10 17:30:24 -0400

    created gradebook.html and js with content

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// TODO: Fetch data from PostgreSQL database (later)
+function fetchGradeData() {
+  console.log("Fetching grade data...");
+}
+
+// TODO: Populate the table with grade data
+function populateGradeTable(data) {
+  console.log("Populating grade table with data...");
+}
+
+// TODO REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradeTable(gradeData);
+// END REMOVE
